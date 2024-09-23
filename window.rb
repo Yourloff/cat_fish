e1414fa7722c3ee61b4fefa27d1cf4ac6da37c9c
Alexey Orloff <alex.yourloff@gmail.com>
2024-09-23 23:31:32 +0700

    Добавление фона. Отключено изменения размера окна. Добавлена кнопка закрыть

--- window.rb
+++ window.rb
@@ -1,15 +1,24 @@
 class SimpleWindow < Gosu::Window
   def initialize(width = 640, height = 480)
-    super(width, height)
-    self.caption = 'gosu!'
+    super(width, height, fullscreen: false, borderless: true)
+    self.caption = 'A cat eats a fish'
+    @background_image = Gosu::Image.new("images/floor.jpg", :tileable => true)
     @font = Gosu::Font.new(20)
     @player = Player.new
     @food = Food.new(width, height)
     @width = width
     @height = height
+
+    @close_button_size = 20
+    @close_button_x = width - @close_button_size - 10
+    @close_button_y = 10
   end
 
   def update
+    if button_down?(Gosu::MsLeft) && over_close_button?(mouse_x, mouse_y)
+      close
+    end
+
     if Gosu.button_down?(Gosu::KB_A)
       @player.move_left
     end
@@ -32,9 +41,12 @@ class SimpleWindow < Gosu::Window
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
     @player.draw
     @food.draw
     @font.draw_text("Счёт: #{@player.score}", 10, 10, 0, 1.0, 1.0, Gosu::Color::YELLOW)
+
+    draw_close_button
   end
 
   def collision?(player, food)
@@ -52,5 +64,22 @@ class SimpleWindow < Gosu::Window
     return false if player_bottom < food_top || player_top > food_bottom
 
     true
+  end
+
+  def over_close_button?(mouse_x, mouse_y)
+    mouse_x >= @close_button_x && mouse_x <= (@close_button_x + @close_button_size) &&
+      mouse_y >= @close_button_y && mouse_y <= (@close_button_y + @close_button_size)
+  end
+
+  # Отрисовка кнопки "Закрыть"
+  def draw_close_button
+    Gosu.draw_rect(@close_button_x, @close_button_y, @close_button_size, @close_button_size, Gosu::Color::RED, 1)
+
+    # Вычисление центра кнопки для размещения "X"
+    x_offset = @close_button_x + (@close_button_size / 4)
+    y_offset = @close_button_y - 1
+
+    # Отрисовка символа "X" по центру кнопки
+    @font.draw_text("X", x_offset, y_offset, 3, 1.0, 1.0, Gosu::Color::WHITE)
   end
 end
